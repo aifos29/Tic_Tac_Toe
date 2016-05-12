8d324e0a39417c82d18057e6499ba243c30ffbbb
aifos29 <sofia.29@hotmail.es>
2016-05-12 02:32:59 -0600

    Clase Auxiliar
    Dicha clase es para crear un objeto para poder valorar el camino a
    seguir por la Computer cuando se juega contra ella

---
+++ Lógica/Varianzas.java
@@ -0,0 +1,16 @@
+package Lógica;
+
+public class Varianzas {
+	public String identificador;
+	public int cont_x;
+	public int cont_o;
+	public int numero;
+	public String dominante;
+	
+	public Varianzas(String id,int numero){
+		identificador=id;
+		this.numero=numero;
+	}
+	
+	
+}
